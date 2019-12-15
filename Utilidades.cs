317a08408e7ab2f1d0996ed31fb95d9d96ce2c21
JarvisFullStack <luis_munoz@ucne.edu.do>
2019-12-14 22:34:41 -0400

    Agregando Toastr

---
+++ Utilidades.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SegundoParcialO2A2
+{
+	public static class Utilidades
+	{
+		public static void ShowToastr(Page page, string message, string title, string type = "info")
+		{
+			page.ClientScript.RegisterStartupScript(page.GetType(), "toastr_message",
+				  String.Format("toastr.{0}('{1}', '{2}');", type.ToLower(), message, title), addScriptTags: true);
+		}
+	}
+}
